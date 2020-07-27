4247cc59f71f121cee14f5cf4bbf288ceaaf8f8a
vicodescity <ojejevictor@gmail.com>
2020-07-27 09:16:30 +0100

    First commit

---
+++ Source/RPG/RPGGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RPGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_API ARPGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
